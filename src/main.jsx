50a2d026022dd7745eda78eb5eccaaaa282989b7
krishmungase <mungasekrishna8@gmail.com>
2024-07-23 18:11:40 +0530

    redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+// import { Provider } from 'react-redux'
+// import store from "./Store/store"
+
+{/* <Provider store={store}>
+<BrowserRouter>
+  <App />
+  <Toaster />
+</BrowserRouter>
+</Provider> */}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
